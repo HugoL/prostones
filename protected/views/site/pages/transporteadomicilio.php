0f485c5c1f84e29c8d7478ccea036a5b71764d4d
Eduardo Asenjo <skuder.zgz@gmail.com>
2014-09-20 03:18:27 +0200

    ss

---
+++ protected/views/site/pages/transporteadomicilio.php
@@ -0,0 +1,79 @@
+
+<?php
+/* @var $this SiteController */
+
+
+$this->pageTitle= 'transporte a domicilio- '. Yii::app()->name . ' Marmolistas';
+
+Yii::app()->clientScript->registerMetaTag("Enviamos mármoles y granitos a buen precio a toda España", 'description');
+Yii::app()->clientScript->registerMetaTag("Entrega, tiempo, Marmol , Granito ,Barato, Precio, Obra, Reforma, Oferta, Baldosa, Rodapie, Pulido, Flameado, Abujardado, Mármol proston, Granito proston,  Marmolistas", 'keywords');
+/*$this->breadcrumbs=array(
+	'Reformas',
+);*/
+?>
+
+
+
+
+<div class="span12">
+	<div class="span12">
+		<div class="span1 iconoser">
+			<img alt="Transporte a domicilio - www.proston.es" src="<?php echo Yii::app()->request->baseUrl.Yii::app()->params['images'] ?>/servicios/transporte.png">
+		</div>
+
+		<h1>Transporte a domicilio.</h1> 
+	</div>
+	
+	<div class="span12">
+		<div class="span6">
+		<p>
+​Le brindamos la posibilidad de cargar su material sobre el camión que usted envie para su recogida, o de entregarle nosotros su Pedido en el lugar y fecha que nos indique.
+<br><br>
+En el caso de que nos confie el envio, la empresa de transporte con la que trabajamos recoge los pallets (en donde se ha embalado su material), en la fábrica o taller de elaboración; posteriormente se lleva al hub mas cercano a su domicilio, para ser entregado por un camión de reparto que cubre la dirección que nos ha indicado.
+<br><br>
+La rapidez en el suministro y la información sobre el transporte de sus pedidos, son una prioridad en proSton.es.  Cuando realice su compra le daremos la posibilidad de hacer el seguimiento de su pedido online...  manteniendo su material bajo control 
+<br><br> 
+Todas las operaciones de distribución de mercancía es sobre pallet y están sometidas a un riguroso Código de Buenas Prácticas, garantizando así una confianza y una profesionalidad sin igual. Intentamos cumplir las necesidades específicas de los clientes proporcionando vehiculos con autodescarga (plataforma elavadora + transpallet).
+</p>
+		</div>
+
+		<div class="span3 divServicio">
+			<div class="span12">
+				<img  alt="Transporte a domicilio - www.proston.es" src="<?php echo Yii::app()->request->baseUrl.Yii::app()->params['images'] ?>/4.jpg">	
+			</div>
+			<div class="span12" align="center"><font size="1">
+				Suministramos a toda España</font>
+			</div>
+			<div class="clearfix">&nbsp</div>
+			
+			
+		</div>
+	</div>
+
+</div>
+
+	<div class="clearfix">&nbsp</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
